ccc1bfa2f7624d5a5b239ddaef2df240a1083a7b
Silvio Doblhofer <silvio.doblhofer@gmail.com>
2016-12-17 16:07:05 +0100

    d3 complete

--- src/aoc/d3.clj
+++ src/aoc/d3.clj
@@ -1,27 +1,38 @@
-(ns aoc.d2
-  (:require [clojure.string :as str])
+(ns aoc.d3
+  (:require [clojure.java.io :as io])
   (:gen-class))
+
+
+(def input (line-seq (io/reader (io/resource "d3"))))
 
 (def input1 "541  588  421
 827  272  126
 660  514  367
  39  703  839")
 
-(defn parse-input [input]
-  (as-> input data
-        (str/split data #"\n")
-        (map #(str/split % #"  ") data)
-        (mapv (partial mapv bigint) data)))
+(defn parse-input [line]
+  (read-string (str "[" line "]")))
+
+        
 
 (defn valid? [[a b c]]
   (and (> (+ a b) c)
        (> (+ a c) b)
        (> (+ b c) a)))
         
+(defn solve [input]
+  (->> input
+       (map parse-input)
+       (filter valid?)
+       count))
 
-(->> input1
-     parse-input
-     (filter valid?)
-     count)
+(defn solvep2 [input]
+  (->> input
+       (map parse-input)
+       (partition 3)
+       (mapcat (partial apply mapv vector))
+       (filter valid?)
+       count))
+(solve input)
+(solvep2 input)
 
-(seq (.getURLs (java.lang.ClassLoader/getSystemClassLoader)))
